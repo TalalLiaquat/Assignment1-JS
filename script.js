0deac42f5f52395f95a6712a6fa634cd63e9a900
M.Talal Liaquat <153552067+TalalLiaquat@users.noreply.github.com>
2024-01-21 00:00:35 -0800

    Add files via upload
    First Assignment of JavaScript (Bano Qabil)

---
+++ script.js
@@ -0,0 +1,10 @@
+window.onload = function() {
+    alert('Welcome to my Page.');
+   };
+
+let age;
+let isStudent;
+age = 22;
+isStudent = true;
+console.log("Age: ", age);
+console.log("Is Student: ", isStudent);
