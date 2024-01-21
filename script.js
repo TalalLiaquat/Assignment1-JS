156160344963bb123b28fe03cd0416398bf52f74
M.Talal Liaquat <153552067+TalalLiaquat@users.noreply.github.com>
2024-01-21 00:06:27 -0800

    Add files via upload
    Assignment 2 of JS

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Assignment 1
 window.onload = function() {
     alert('Welcome to my Page.');
    };
@@ -8,3 +9,13 @@ age = 22;
 isStudent = true;
 console.log("Age: ", age);
 console.log("Is Student: ", isStudent);
+
+
+//Assignment 2
+let length = 5;
+let width = 5;
+
+let area = length * width;
+
+console.log("The area of the rectangle is: " + area);
+
